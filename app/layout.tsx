dca8da5cb6a58bc57be441e06babbb2982558877
ceeji <i@ceeji.net>
2025-01-10 19:49:31 +0800

    增加统计代码

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,9 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "AI Chat Web",
-  description: "Your personal AI Chat Bot.",
+  title: "百言 AI",
+  description:
+    "专业 AI 创作工具，快速用上世界先进 AI，极大提高生活工作效率。支持各类高质量模型，助你创造无与伦比的内容。",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -18,7 +19,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "AI Chat Web",
+    title: "百言 AI",
     statusBarStyle: "default",
   },
 };
@@ -34,6 +35,18 @@ export default function RootLayout({
         <meta name="config" content={JSON.stringify(getClientConfig())} />
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
+        <script
+          charSet="UTF-8"
+          id="LA_COLLECT"
+          src="//sdk.51.la/js-sdk-pro.min.js"
+          defer
+        ></script>
+        <script
+          defer
+          dangerouslySetInnerHTML={{
+            __html: `LA.init({id:"3Ktn7HQCjAwa7NMl",ck:"3Ktn7HQCjAwa7NMl",autoTrack:true,hashMode:true})`,
+          }}
+        />
       </head>
       <body>{children}</body>
     </html>
